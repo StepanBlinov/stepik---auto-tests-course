3dd060daa96e0afdb76cd50c2a29abdea369c6d4
s.blinov <s.blinov@corp.mail.ru>
2022-04-22 15:10:48 +0300

    новый тест

--- lesson10_step8.py
+++ lesson10_step8.py
@@ -10,7 +10,7 @@ def calc(x):#функция для вычисления y
 browser = webdriver.Chrome()
 
 browser.get("http://suninjuly.github.io/explicit_wait2.html")
-
+3
 # говорим Selenium проверять в течение 12 секунд, пока значение элемента price не будет равнно 100
 WebDriverWait(browser, 12).until(
         EC.text_to_be_present_in_element((By.ID, "price"),"100"))
