de773a2850d5c02ffa02dbe69e1cabe9d2465e4f
s.blinov <s.blinov@corp.mail.ru>
2022-04-22 15:31:37 +0300

    мерж

--- lesson10_step8.py
+++ lesson10_step8.py
@@ -3,14 +3,14 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 import math
-
+"тут"
 def calc(x):#функция для вычисления y
   return str(math.log(abs(12*math.sin(int(x)))))
 
 browser = webdriver.Chrome()
 
 browser.get("http://suninjuly.github.io/explicit_wait2.html")
-3
+
 # говорим Selenium проверять в течение 12 секунд, пока значение элемента price не будет равнно 100
 WebDriverWait(browser, 12).until(
         EC.text_to_be_present_in_element((By.ID, "price"),"100"))
